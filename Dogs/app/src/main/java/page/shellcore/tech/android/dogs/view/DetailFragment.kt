887b00265a96d8d190353c150c262172510619d6
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-04-21 18:52:59 -0500

    Se agregó botones para navegación entre los fragmentos.

--- Dogs/app/src/main/java/page/shellcore/tech/android/dogs/view/DetailFragment.kt
+++ Dogs/app/src/main/java/page/shellcore/tech/android/dogs/view/DetailFragment.kt
@@ -1,8 +1,21 @@
 package page.shellcore.tech.android.dogs.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_detail.*
 import page.shellcore.tech.android.dogs.R
 
 class DetailFragment : Fragment(R.layout.fragment_detail) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnList.setOnClickListener {
+            val action = DetailFragmentDirections.actionListFragment()
+            Navigation.findNavController(it)
+                .navigate(action)
+        }
+    }
 }
--- Dogs/app/src/main/java/page/shellcore/tech/android/dogs/view/ListFragment.kt
+++ Dogs/app/src/main/java/page/shellcore/tech/android/dogs/view/ListFragment.kt
@@ -1,8 +1,22 @@
 package page.shellcore.tech.android.dogs.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_list.*
 import page.shellcore.tech.android.dogs.R
 
 class ListFragment : Fragment(R.layout.fragment_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnDetails.setOnClickListener {
+            val action = ListFragmentDirections.actionDetailFragment()
+            Navigation.findNavController(it)
+                .navigate(action)
+        }
+    }
 
 }
