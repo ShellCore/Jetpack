d4cda18dde6a83e3253c7b22543c726ba5d7e1b1
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-04-22 15:27:07 -0500

    Creación del modelo Perro para uso en la aplicación.

---
+++ Dogs/app/src/main/java/page/shellcore/tech/android/dogs/model/Model.kt
@@ -0,0 +1,11 @@
+package page.shellcore.tech.android.dogs.model
+
+data class DogBreed(
+    val breedId: String?,
+    val dogBreed: String?,
+    val lifeSpan: String?,
+    val breedGroup: String?,
+    val bredFor: String?,
+    val temperament: String?,
+    val imageUrl: String?
+)
