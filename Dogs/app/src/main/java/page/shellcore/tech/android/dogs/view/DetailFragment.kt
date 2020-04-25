a6e2e5c2e2e4718f58daa38f593040e19e19fc4d
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-04-25 07:18:41 -0500

    Se agregó binding de datos en el detalle del perro.

--- Dogs/app/src/main/java/page/shellcore/tech/android/dogs/view/DetailFragment.kt
+++ Dogs/app/src/main/java/page/shellcore/tech/android/dogs/view/DetailFragment.kt
@@ -1,20 +1,35 @@
 package page.shellcore.tech.android.dogs.view
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.fragment_detail.*
 import page.shellcore.tech.android.dogs.R
+import page.shellcore.tech.android.dogs.databinding.FragmentDetailBinding
 import page.shellcore.tech.android.dogs.util.getProgressDrawable
 import page.shellcore.tech.android.dogs.util.loadImage
 import page.shellcore.tech.android.dogs.viewmodel.DetailViewModel
 
-class   DetailFragment : Fragment(R.layout.fragment_detail) {
+class   DetailFragment : Fragment() {
+
+    private lateinit var dataBinding: FragmentDetailBinding
 
     private lateinit var viewModel: DetailViewModel
     private var dogUuid = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        return dataBinding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -32,13 +47,7 @@ class   DetailFragment : Fragment(R.layout.fragment_detail) {
     private fun observeViewModel() {
         viewModel.dogLiveData.observe(viewLifecycleOwner, Observer { dog ->
             dog?.let { dogData ->
-                detailTxtName.text = dogData.dogBreed
-                detailTxtPurpose.text = dogData.bredFor
-                detailTxtTemperament.text = dogData.temperament
-                detailTxtLifespan.text = dogData.lifeSpan
-                context?.let {
-                    detailImgDog.loadImage(dogData.imageUrl, getProgressDrawable(it))
-                }
+                dataBinding.dog = dogData
             }
         })
     }
